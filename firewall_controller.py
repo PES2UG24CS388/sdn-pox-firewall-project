4d24db3e81ea167970e820165d632f0a42411e61
R.Pooja <flowerblossom2k06@gmail.com>
2026-04-19 21:47:19 +0530

    Update firewall_controller.py

--- firewall_controller.py
+++ firewall_controller.py
@@ -9,7 +9,7 @@ blocked_dst = "00:00:00:00:00:03"
 
 
 def _handle_ConnectionUp(event):
-    log.info("Switch connected")
+    log.info("Switch connected: %s", event.connection)
 
 
 def _handle_PacketIn(event):
@@ -18,26 +18,31 @@ def _handle_PacketIn(event):
     dst = str(packet.dst)
     in_port = event.port
 
+    # Learn MAC
     mac_to_port[src] = in_port
 
-    # 🚫 Block h1 -> h3
+    # 🔍 PATH TRACING
+    log.info("Packet path: %s -> s1 -> %s", src, dst)
+
+    # 🚫 BLOCK RULE
     if src == blocked_src and dst == blocked_dst:
         log.info("Blocked traffic from %s to %s", src, dst)
         return
 
-    # If destination known, install flow rule
+    # ✅ FLOW RULE INSTALLATION
     if dst in mac_to_port:
         out_port = mac_to_port[dst]
 
         msg = of.ofp_flow_mod()
         msg.match.dl_dst = packet.dst
         msg.actions.append(of.ofp_action_output(port=out_port))
+
         event.connection.send(msg)
 
-        log.info("Installed flow %s -> port %s", dst, out_port)
+        log.info("Flow installed: %s -> port %s", dst, out_port)
 
     else:
-        # flood unknown
+        # flood if unknown
         msg = of.ofp_packet_out()
         msg.data = event.ofp
         msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
